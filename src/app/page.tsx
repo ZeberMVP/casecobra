214e2ab6f666e192d4b1c050d1b3ebad56bbd2e0
ZeberMVP <rubeen722@gmail.com>
2024-07-09 00:17:42 +0200

    upload your photo banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -206,6 +206,40 @@ export default function Home() {
           <Reviews />
         </div>
       </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 text-balance text-center text-5xl font-bold !leading-tight tracking-tight text-gray-900 md:text-6xl">
+                Upload your photo and get{" "}
+                <span className="relative bg-green-600 px-2 text-white">
+                  your own case
+                </span>
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex grid-cols-2 flex-col items-center gap-40 md:grid">
+              <img
+                src="/arrow.png"
+                alt=""
+                className="absolute left-1/2 top-[25rem] z-10 -translate-x-1/2 -translate-y-1/2 rotate-90 md:top-1/2 md:rotate-0"
+              />
+
+              <div className="relative h-80 w-full max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 md:h-full md:justify-self-end lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  alt=""
+                  className="h-full w-full rounded-md bg-white object-cover shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
     </div>
   );
 }
